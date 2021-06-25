e8c14560aa93ab4c742266fe1b18193f11f57654
nelgoez <76503414+nelgoez@users.noreply.github.com>
2021-06-24 23:26:09 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import store from './redux/store/store';
 
 ReactDOM.render(
   <Provider store={store}>
+    <React.StrictMode>
       <App />
+    </React.StrictMode>
   </Provider>,
   document.getElementById('root')
 );
@@ -16,4 +18,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
